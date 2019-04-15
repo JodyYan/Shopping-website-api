f6bc43d7bac3c777575d4847d9ac875fb4bb78e4
JodyYan <stu891272004@yahoo.com.tw>
2019-04-15 15:27:32 +0800

    seller registration

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Seller;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
 {
-    //
+    public function store()
+    {
+        return Seller::create(request()->all());
+    }
 }
