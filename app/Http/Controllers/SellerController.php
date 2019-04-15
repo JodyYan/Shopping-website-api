7769e95142691119a108ec238f1c7ce8f2479b0c
JodyYan <stu891272004@yahoo.com.tw>
2019-04-15 17:43:40 +0800

    seller login

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use App\Seller;
 use Illuminate\Http\Request;
 
@@ -10,5 +11,18 @@ class SellerController extends Controller
     public function store()
     {
         return Seller::create(request()->all());
+    }
+
+    public function login()
+    {
+        $i=request()->get('email');
+        $q=request()->get('password');
+        if($s=Seller::where('email', $i)->where('password', $q)->first()) {
+            $token = Str::random(60);
+            $s->api_token=$token;
+            $s->save();
+            return $s;
+        }
+
     }
 }
