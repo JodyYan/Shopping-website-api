fc723f16397272f6bba6f102b27fd3e5cb2cef59
JodyYan <stu891272004@yahoo.com.tw>
2019-04-22 15:09:04 +0800

    create shopping cart tables

---
+++ database/migrations/2019_04_22_142311_create_shoppings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShoppingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shoppings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->string('adress');
+            $table->Integer('total_price');
+            $table->bigInteger('seller_id');
+            $table->bigInteger('buyer_id');
+            $table->boolean('buyer_delete');
+            $table->boolean('seller_delete');
+            $table->Integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shoppings');
+    }
+}
